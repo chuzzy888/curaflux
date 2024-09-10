75cb7b83feeb6d5b405b7a252d40cbf5672136ce
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-09-10 19:09:14 +0100

    feat: added shadCN for ui

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ScreenLayout } from "../../components/layout/ScreenLayout";
+import { Button } from "../../components/ui/button";
 
 const Login = () => {
   return (
@@ -8,10 +9,18 @@ const Login = () => {
         <section>img</section>
 
         <section>
+          <form>
           <h1 className="text-xl font-bold">Welcome Back! Sign In</h1>
-          <button>Google</button>
+          <button type="button">Google</button>
 
           <div className=" h-[0.5px] w-full my-7 bg-gray-300" />
+
+            <div>
+                <label htmlFor="" className=" font-semibold text-sm">Email or Phone number</label>
+
+                <Button>me</Button>
+            </div>
+          </form>
         </section>
       </main>
     </ScreenLayout>
