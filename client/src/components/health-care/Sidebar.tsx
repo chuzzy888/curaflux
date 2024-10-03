eb76502eb949e322c5fcafe065f5a0a440d3893e
Your Name <chizupraise31@gmail.com>
2024-10-03 14:06:34 -0700

    modified admin

--- client/src/components/health-care/Sidebar.tsx
+++ client/src/components/health-care/Sidebar.tsx
@@ -3,6 +3,8 @@ import { MdSpaceDashboard } from "react-icons/md";
 import { TbBriefcaseFilled } from "react-icons/tb";
 import { BiSolidMessageDetail } from "react-icons/bi";
 import { IoSettings } from "react-icons/io5";
+import { SiCoderwall } from "react-icons/si";
+
 // import { MdLogout } from "react-icons/md";
 import { Link } from "react-router-dom";
 // import Cookies from "js-cookie";
@@ -15,8 +17,6 @@ import { Link } from "react-router-dom";
 export default function Sidebar() {
   // const token = Cookies.get("healthcareToken");
   // const decode = token ? jwtDecode<CustomJwtPayload>(token) : null;
-
-
 
   return (
     <div className="w-64 bg-gray-100 h-screen fixed top-0 left-0">
@@ -33,15 +33,7 @@ export default function Sidebar() {
           </p>
           Dashboard
         </Link>
-        <Link
-          to={`/curaflux/healthcare/admin/all-shift`}
-          className="flex items-center mx-3 py-2 px-4 text-gray-700 rounded-md hover:bg-gray-200"
-        >
-          <p className="mr-3">
-            <MdSpaceDashboard className="text-xl" />
-          </p>
-          All Shifts
-        </Link>
+
         <Link
           to={"#"}
           className="flex items-center mx-3 py-2 px-4 text-gray-700 rounded-md hover:bg-gray-200"
@@ -51,6 +43,7 @@ export default function Sidebar() {
           </p>
           Locums
         </Link>
+
         <Link
           to={""}
           className="flex items-center mx-3 py-2 px-4 text-gray-700 rounded-md hover:bg-gray-200"
@@ -59,6 +52,15 @@ export default function Sidebar() {
             <BiSolidMessageDetail className="text-xl" />
           </p>
           Applications
+        </Link>
+        <Link
+          to={`/curaflux/healthcare/admin/all-shift`}
+          className="flex items-center mx-3 py-2 px-4 text-gray-700 rounded-md hover:bg-gray-200"
+        >
+          <p className="mr-3">
+            <SiCoderwall className="text-xl" />
+          </p>
+          Shift Overview
         </Link>
         <Link
           to={"#"}
@@ -70,9 +72,7 @@ export default function Sidebar() {
           Settings
         </Link>
       </nav>
-      <div className="absolute bottom-0 left-0 w-full p-4">
-     
-      </div>
+      <div className="absolute bottom-0 left-0 w-full p-4"></div>
     </div>
   );
 }
